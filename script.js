e4ee3453a5090649e0d4d7244d98b237ab642528
Josh Carroccia <agr.carroccia@gmail.com>
2018-08-08 16:29:36 -0400

    added styling

--- script.js
+++ script.js
@@ -13,67 +13,79 @@ var equal = document.getElementById('equal')
 var inputs = document.getElementById('inputs')
 var clear = document.getElementById('clear')
 var inputStr = "";
-one.addEventListener('click', function addInput1(){
+one.addEventListener('click', function add(){
     inputs.innerHTML += "1"
     inputStr += 1
 });
-two.addEventListener('click', function addInput2(){
+two.addEventListener('click', function add(){
     inputs.innerHTML += "2"
     inputStr += 2
 });
-three.addEventListener('click', function addInput2(){
+three.addEventListener('click', function add(){
     inputs.innerHTML += "3"
     inputStr += 3
 });
-four.addEventListener('click', function addInput2(){
+four.addEventListener('click', function add(){
     inputs.innerHTML += "4"
     inputStr += 4
 });
-five.addEventListener('click', function addInput2(){
+five.addEventListener('click', function add(){
     inputs.innerHTML += "5"
     inputStr += 5
 });
-six.addEventListener('click', function addInput2(){
+six.addEventListener('click', function add(){
     inputs.innerHTML += "6"
     inputStr += 6
 });
-seven.addEventListener('click', function addInput2(){
+seven.addEventListener('click', function add(){
     inputs.innerHTML += "7"
     inputStr += 7
 });
-eight.addEventListener('click', function addInput2(){
+eight.addEventListener('click', function add(){
     inputs.innerHTML += "8"
     inputStr += 8
 });
-nine.addEventListener('click', function addInput2(){
+nine.addEventListener('click', function add(){
     inputs.innerHTML += "9"
     inputStr += 9
 });
-zero.addEventListener('click', function addInput2(){
+zero.addEventListener('click', function add(){
     inputs.innerHTML += "0"
     inputStr += 0
 });
-addition.addEventListener('click', function addInputAdd(){
+addition.addEventListener('click', function add(){
     inputs.innerHTML += "+"
     inputStr += "+"
 });
-sub.addEventListener('click', function addInputAdd(){
+sub.addEventListener('click', function add(){
     inputs.innerHTML += "-"
     inputStr += "-"
 });
-divide.addEventListener('click', function addInputAdd(){
+divide.addEventListener('click', function add(){
     inputs.innerHTML += "/"
     inputStr += "/"
 });
-multiply.addEventListener('click', function addInputAdd(){
+multiply.addEventListener('click', function add(){
     inputs.innerHTML += "*"
     inputStr += "*"
 });
-equal.addEventListener('click', function runInputs(){
+decimal.addEventListener('click', function add(){
+    inputs.innerHTML += "."
+    inputStr += "."
+});
+lPara.addEventListener('click', function add(){
+    inputs.innerHTML += "("
+    inputStr += "("
+});
+rPara.addEventListener('click', function add(){
+    inputs.innerHTML += ")"
+    inputStr += ")"
+});
+equal.addEventListener('click', function add(){
     inputs.innerHTML = eval(inputStr)
 });
 
-clear.addEventListener('click', function runInputs(){
+clear.addEventListener('click', function add(){
     inputs.innerHTML = ''
     inputStr = ''
 });
